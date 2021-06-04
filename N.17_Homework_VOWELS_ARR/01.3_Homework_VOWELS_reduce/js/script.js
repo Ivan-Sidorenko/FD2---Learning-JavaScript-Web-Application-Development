e80c9c8a46fe46a68af621b1e6b086928311551b
ivan sidorenko <ivan.a.sidorenko@gmail.com>
2021-06-04 10:32:22 +0300

    feat: new solution on task N.20 - 01.3 reduce

--- N.17_Homework_VOWELS_ARR/01.3_Homework_VOWELS_reduce/js/script.js
+++ N.17_Homework_VOWELS_ARR/01.3_Homework_VOWELS_reduce/js/script.js
@@ -3,10 +3,8 @@
 var uString = prompt("Введите строку");
 
 function countVowels(simpleString, stringVowel = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]) {
-  return simpleString
-  .toLowerCase()
-  .split("")
-  .reduce((sum, symbol) => stringVowel.includes(symbol) ? sum += symbol : sum, "").length;
+  let count = [...simpleString].reduce((sum, symbol) => stringVowel.includes(symbol) ? sum + 1 : sum, 0);
+  return count;
 }
 
 alert(countVowels(uString));
